07257f9c4e3e8d2ab013a6f17972fb89380f6fbd
Andrew <terminatordog0000@gmail.com>
2023-11-12 19:49:07 -0500

    Added regex match to moves

--- townService/src/town/chess/chess_app.ts
+++ townService/src/town/chess/chess_app.ts
@@ -1,12 +1,17 @@
-// just filling for github to make the directory
+/**
+ * TO DO:
+ * Implement regex matching for moves passed to function from front end to match move in movelist
+ * Make timer count down
+ */
+
 import { Chess } from 'chess.js';
 
 // const chess = new Chess();
 // Class for Chess Game
 
 export enum Colors {
-  White = 'White',
-  Black = 'Black',
+  White = 'w',
+  Black = 'b',
 }
 
 class ChessGame {
@@ -18,13 +23,10 @@ class ChessGame {
   // black_id: number; black player id
   timer: number;
 
-  private _currTurn: Colors; // White / Black
-
   constructor(time_limit: number) {
     this.gameID = Math.floor(Math.random() * 100000);
     this.timer = time_limit;
     this._game = new Chess();
-    this._currTurn = Colors.White;
   }
 
   private _preMoveChecks(): boolean {
@@ -43,8 +45,8 @@ class ChessGame {
   public make_move(moveToMake: string, color: Colors): void {
     if (
       this._preMoveChecks() &&
-      this._game.moves().includes(moveToMake) &&
-      color === this._currTurn
+      (this._game.moves().includes(moveToMake) || this.matchMoves(moveToMake) !== 'None') &&
+      color === this._game.turn()
     ) {
       this._game.move(moveToMake);
     } else {
@@ -55,12 +57,39 @@ class ChessGame {
   public getFen(): string {
     return this._game.fen();
   }
+
+  public getMoves(): string[] {
+    return this._game.moves();
+  }
+
+  public matchMoves(moveToMake: string): string {
+    const moves = this.getMoves();
+    for (let i = 0; i < moves.length; i++) {
+      const move = moves[i];
+      console.log(move);
+      if (move.match(`^${moveToMake}.?`)) {
+        return move;
+      }
+    }
+    return 'None';
+  }
+
+  public getTurn(): string {
+    return this._game.turn();
+  }
 }
 
 const gamer = new ChessGame(10);
 console.log(gamer.gameID);
 console.log(gamer.getFen());
-gamer.make_move('e4', Colors.White);
+console.log(gamer.getMoves());
+const MOVE = 'e3#';
+console.log(gamer.matchMoves(MOVE));
+// if ('e3+'.match(`^${MOVE}`)) {
+//   console.log('true');
+// }
+gamer.make_move('Na3', Colors.White);
 console.log(gamer.getFen());
+console.log(gamer.getTurn() === Colors.Black);
 
 export default ChessGame;
