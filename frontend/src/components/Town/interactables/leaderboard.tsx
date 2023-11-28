316d1786047919e0f0d3087d18d2db7bd609a655
Brian Bates <124414764+brianbates928@users.noreply.github.com>
2023-11-28 13:21:50 -0500

    Update leaderboard.tsx
    changed to work with database connection

--- frontend/src/components/Town/interactables/leaderboard.tsx
+++ frontend/src/components/Town/interactables/leaderboard.tsx
@@ -1,28 +1,21 @@
+import { Table, Tbody, Td, Thead, Tr } from '@chakra-ui/react';
 import React from 'react';
 import { GameResult } from '../../../types/CoveyTownSocket';
-import updateElo from Elo.ts
+import  leaderboardElo, updateElo from elo.ts
 //very basic leaderboard will continue to update
 
 export default function Leaderboard({ results }: { results: GameResult[] }): JSX.Element {
-  // Create a map to store aggregated results for each player
-  // come back and connect to database once fully set up
+// get players for update leaderbaord
   var currentplayers = new Array();
-  const playerStats: { [playerName: string]: { elo: number } } = {};
-
   results.forEach(result => {
     for (const [playerName] of Object.entries(result.scores)) {
-      if (!playerStats[playerName]) {
-        playerStats[playerName] = { elo: 1500 };
-      }
       currentplayers.push(playerName);
     }
   });
-  updateElo(currentplayers[0],currentplayers[1], results) //update elo from elo.ts
-  // Convert the playerStats map to an array and sort by elo, come back to grabbing from database when set up
-  const sortedPlayers = Object.entries(playerStats)
-    .map(([playerName, stats]) => ({ playerName, ...stats }))
-    .sort((a, b) => b.elo - a.elo);
-  //display leaderboard
+  updateElo(currentplayers[0],currentplayers[1], results) //update elo from elo.ts with current players
+  // take leaderboard map
+  const currentleaderboard: { [username: string]: any } = leaderboardElo()
+  //display name: elo
   return (
     <table role='grid'>
       <thead>
@@ -32,10 +25,10 @@ export default function Leaderboard({ results }: { results: GameResult[] }): JSX
         </tr>
       </thead>
       <tbody>
-        {sortedPlayers.map(player => (
-          <tr role='row' key={player.playerName}>
-            <td role='gridcell'>{player.playerName}</td>
-            <td role='gridcell'>{player.elo}</td>
+      {Object.entries(currentleaderboard).map(([playerName, elo]) => (
+          <tr role='row' key={playerName}>
+            <td role='gridcell'>{playerName}</td>
+            <td role='gridcell'>{elo}</td>
  
           </tr>
         ))}
