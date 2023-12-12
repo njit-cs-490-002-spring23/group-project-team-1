9f824295faee3e3460b4ef7988a569ff294a8f29
Nhanmo Flomo <njf9@njit.edu>
2023-12-12 09:46:45 -0500

    Making progress on frontend. UI changes when game is selected, currently shows non interactable chessboard and options to play a human or AI.

--- frontend/src/components/Town/interactables/NewGameModal.tsx
+++ frontend/src/components/Town/interactables/NewGameModal.tsx
@@ -13,22 +13,20 @@ import {
   useToast,
 } from '@chakra-ui/react';
 import React, { useCallback, useEffect, useState } from 'react';
+import { Chessboard } from 'react-chessboard';
 import { useInteractable } from '../../../classes/TownController';
 import { GameArea } from '../../../generated/client';
 import useTownController from '../../../hooks/useTownController';
-//import Time from './time';
-//import { Chessboard } from 'react-chessboard'; //https://medium.com/@ryangregorydev/creating-a-chessboard-app-in-react-3fd9e2b2f6a6
-// eslint-disable-next-line import/no-extraneous-dependencies
-//import { Chess, validateFen } from 'chess.js';
 //import axios from 'axios';
 
 export default function NewGameModal(): JSX.Element {
   const coveyTownController = useTownController();
   const newGame = useInteractable('gameArea');
   const [chosenGame, setChosenGame] = useState<string>('');
+  const [game, setGame] = useState(' a Game');
+  const [currentUI, setUI] = useState('General');
 
   const isOpen = newGame !== undefined;
-
   useEffect(() => {
     if (newGame) {
       coveyTownController.pause();
@@ -39,11 +37,63 @@ export default function NewGameModal(): JSX.Element {
 
   const closeModal = useCallback(() => {
     if (newGame) {
+      setGame(' a Game');
+      setChosenGame('');
       coveyTownController.interactEnd(newGame);
     }
   }, [coveyTownController, newGame]);
 
   const toast = useToast();
+
+  function ChessContent() {
+    return (
+      <ModalContent display={'flex'} flexDirection='row'>
+        <ModalContent>
+          <ModalHeader textAlign={'center'}>
+            Welcome to chess, {coveyTownController.ourPlayer.userName}!
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Chessboard id='BasicBoard' arePiecesDraggable={false} />
+          </ModalBody>
+          <ChessFooter />
+        </ModalContent>
+        <ModalContent>
+          <ModalContent>
+            <ModalHeader textAlign={'center'}>Current Players</ModalHeader>
+            <ModalBody textAlign={'center'}></ModalBody>
+          </ModalContent>
+        </ModalContent>
+      </ModalContent>
+    );
+  }
+
+  function ChessFooter() {
+    return (
+      <ModalFooter display='flex' flexDirection={'column'} padding={1}>
+        <ModalFooter justifyItems={'center'}>
+          <Button size='sm' marginRight={5}>
+            Play an AI
+          </Button>
+          <Button size='sm' marginRight={5}>
+            Play a Human
+          </Button>
+          <Button size='sm'>Leaderboard</Button>
+        </ModalFooter>
+        <ModalFooter alignSelf={'end'}>
+          <Button
+            size='sm'
+            colorScheme='red'
+            onClick={() => {
+              closeModal();
+              setUI('General');
+            }}>
+            Leave
+          </Button>
+        </ModalFooter>
+      </ModalFooter>
+    );
+  }
 
   const createGame = useCallback(async () => {
     if (chosenGame && newGame) {
@@ -58,29 +108,9 @@ export default function NewGameModal(): JSX.Element {
           title: 'Game Created!',
           status: 'success',
         });
+        setUI(chosenGame);
+        setGame('a Game');
         setChosenGame('');
-        closeModal();
-        coveyTownController.pause();
-        console.log(coveyTownController.paused);
-        return (
-          <Modal
-            isOpen={isOpen}
-            onClose={() => {
-              closeModal();
-              coveyTownController.unPause();
-            }}>
-            <ModalOverlay />
-            <ModalContent>
-              <ModalHeader>Welcome to Chess, {coveyTownController.ourPlayer.id}!</ModalHeader>
-              <ModalCloseButton />
-              <ModalBody>
-                <Button size='md'>Leave</Button>
-                <Button size='sm'>Play Against an AI</Button>
-                <Button size='sm'>Wait Another Player</Button>
-              </ModalBody>
-            </ModalContent>
-          </Modal>
-        );
       } catch (err) {
         if (err instanceof Error) {
           toast({
@@ -99,8 +129,52 @@ export default function NewGameModal(): JSX.Element {
     }
   }, [chosenGame, setChosenGame, coveyTownController, newGame, closeModal, toast]);
 
+  function GameContent() {
+    if (currentUI == 'General') {
+      return (
+        <>
+          <ModalHeader>Create a game in {newGame?.name}</ModalHeader>
+          <ModalCloseButton />
+          <form
+            onSubmit={ev => {
+              ev.preventDefault();
+              createGame();
+            }}>
+            <ModalBody pb={6}>
+              <FormControl>
+                <FormLabel htmlFor='game'>Choose a Game to Play</FormLabel>
+                <Select
+                  isRequired={true}
+                  placeholder='Choose game...'
+                  id='game'
+                  name='game'
+                  value={chosenGame}
+                  onChange={e => {
+                    setChosenGame(e.target.value);
+                    if (e.target.value) setGame(e.target.value);
+                    else setGame(' a Game');
+                  }}>
+                  <option>Chess</option>
+                </Select>
+              </FormControl>
+            </ModalBody>
+            <ModalFooter>
+              <Button colorScheme='blue' mr={3} onClick={createGame}>
+                Play {game}
+              </Button>
+              <Button onClick={closeModal}>Cancel</Button>
+            </ModalFooter>
+          </form>
+        </>
+      );
+    } else {
+      return ChessContent();
+    }
+  }
+
   return (
     <Modal
+      id='chessUI'
       isOpen={isOpen}
       onClose={() => {
         closeModal();
@@ -108,34 +182,7 @@ export default function NewGameModal(): JSX.Element {
       }}>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>Create a game in {newGame?.name} </ModalHeader>
-        <ModalCloseButton />
-        <form
-          onSubmit={ev => {
-            ev.preventDefault();
-            console.log("This one was called.");
-            createGame();
-          }}>
-          <ModalBody pb={6}>
-            <FormControl>
-              <FormLabel htmlFor='game'>Choose a Game to Play</FormLabel>
-              <Select
-                placeholder='Choose game...'
-                id='game'
-                name='game'
-                value={chosenGame}
-                onChange={e => setChosenGame(e.target.value)}>
-                <option>Chess</option>
-              </Select>
-            </FormControl>
-          </ModalBody>
-          <ModalFooter>
-            <Button colorScheme='blue' mr={3} onClick={createGame}>
-              Create
-            </Button>
-            <Button onClick={closeModal}>Cancel</Button>
-          </ModalFooter>
-        </form>
+        <GameContent />
       </ModalContent>
     </Modal>
   );
