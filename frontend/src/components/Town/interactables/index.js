756cb0d6e0f6d0851fc45fd2c215301a5518a1b8
Andrew <terminatordog0000@gmail.com>
2023-12-12 23:06:01 -0500

    ESLINT!!!!

--- frontend/src/components/Town/interactables/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import Time from "./time";
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<Time />, rootElement);
